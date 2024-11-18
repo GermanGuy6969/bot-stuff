bca41e028af60cdabf6dbe43f8c297faa8dd08ee
German Guy <139156300+GermanGuy6969@users.noreply.github.com>
2024-11-18 13:55:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["hi im a bot", "why are you looking at me?"];
+const statusMessages = ["hi im a bot", "why are you looking at me?", "You're still looking at me"];
 const statusTypes = [ 'dnd', 'idle', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
